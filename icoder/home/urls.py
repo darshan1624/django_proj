300ede1e7f912de6330f78a19ae67c80e23a2ece
Darshan <darshan@vectorconsulting.in>
2024-11-14 22:16:06 +0530

    Connection to postgresql pgadmin

--- icoder/home/urls.py
+++ icoder/home/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about')
-}
+]
--- icoder/icoder/settings.py
+++ icoder/icoder/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'icoder.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "blog_project",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
